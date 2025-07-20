3b0404c45b7c0777c1a3830abdea344b88d38c6a
Stefany <stefanypas123@hotmail.com>
2025-07-20 16:42:16 -0300

    [Update] desenvolvimento login

--- src/main/resources/static/js/load_footer.js
+++ src/main/resources/static/js/load_footer.js
@@ -1,5 +1,7 @@
-fetch('../../templates/includes/footer.html')
+fetch('includes/footer.html')
     .then(response => response.text())
     .then(data => {
         document.getElementById('footer').innerHTML = data;
-    });
+    
+    })
+    .catch(error => console.error('Erro ao carregar footer:', error));
